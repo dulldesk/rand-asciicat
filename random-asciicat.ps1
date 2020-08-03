b55d23d1596da05a4cdbfdacea1a9c2c5e9d0dca
dulldesk <54601393+dulldesk@users.noreply.github.com>
2020-08-03 11:38:38 -0400

    Add files via upload

---
+++ random-asciicat.ps1
@@ -0,0 +1,14 @@
+[string[]]$lines = cat $PSScriptRoot\asciicat.txt
+
+$cattot = $lines.length - ($lines | measure -line).lines +1
+$catno = [math]::floor((random -min 0 -max $cattot))
+
+# get starting index
+$ind = 0
+for ($i=1;$i -le $catno; $i++) {
+	$ind = [array]::IndexOf($lines,'',$ind)+1;
+}
+
+for ($i = $ind; $i -lt $lines.length -and $lines[$i].trim() -ne ''; $i++) {
+	write-output $lines[$i]
+}
