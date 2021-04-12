6dfff0e97873005d8c7df4461786f279875e4d4a
dulldesk <54601393+dulldesk@users.noreply.github.com>
2021-04-12 18:38:17 -0400

    Rewrite script

--- random-asciicat.ps1
+++ random-asciicat.ps1
@@ -1,14 +1,1 @@
-[string[]]$lines = cat $PSScriptRoot\asciicat.txt -Encoding UTF8
-
-$cattot = $lines.length - ($lines | measure -line).lines +1
-$catno = [math]::floor((random -min 0 -max $cattot))
-
-# get starting index
-$ind = 0
-for ($i=1;$i -le $catno; $i++) {
-	$ind = [array]::IndexOf($lines,'',$ind)+1;
-}
-
-for ($i = $ind; $i -lt $lines.length -and $lines[$i].trim() -ne ''; $i++) {
-	write-output $lines[$i]
-}
+random (((cat $PSScriptRoot\asciicat.txt -encoding utf8).replace("`r","`n") -join "`n") -split "`n{2,}")
