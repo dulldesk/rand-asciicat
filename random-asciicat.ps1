334991a653425facb33aef84f051dcf59864091d
dulldesk <54601393+dulldesk@users.noreply.github.com>
2025-01-26 00:50:37 -0500

    resolve aliases

--- random-asciicat.ps1
+++ random-asciicat.ps1
@@ -1,1 +1,1 @@
-random (((cat $PSScriptRoot\asciicat.txt -encoding utf8).replace("`r","`n") -join "`n") -split "`n{2,}")
+get-random (((get-content $PSScriptRoot\asciicat.txt -encoding utf8).replace("`r","`n") -join "`n") -split "`n{2,}")
